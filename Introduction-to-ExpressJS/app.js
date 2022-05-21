5f6ad2fcd9601c4cc04fe52578c5e85a81f4f463
Raihan567 <aburaihansarker567@gmail.com>
2022-05-21 14:24:24 +0600

    learning nodejs

---
+++ Introduction-to-ExpressJS/app.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+app.use(morgan("dev"));
+
+app.get("/pany", (req, res) => {
+  res.send("<h1>Hi, I am pany Page</h1>");
+});
+app.get("/get", (req, res) => {
+  res.send("<h1>Hi, I am get Page</h1>");
+});
+app.get("/help", (req, res) => {
+  res.send("<h1>Hi, I am help Page</h1>");
+});
+
+app.get("/service", (req, res) => {
+  res.send("<h1>Hello, I am service page</h1>");
+});
+
+app.get("/about", (req, res) => {
+  // res.send("<h1>Hi I am about page.</h1>");
+  res.json({
+    message: "Hello nodejs how are you. I am Looking for you. ",
+  });
+});
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, Raihan I am NodeJs. Keep learning </h1>");
+});
+app.get("*", (req, res) => {
+  res.send("<h1>Page not Found, 404</h1>");
+});
+const Port = process.env.Port || 7070;
+app.listen(Port, () => {
+  console.log("Server is running on port 7070");
+});
